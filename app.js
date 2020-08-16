f07e51a10e7ad1f6df0ed2e955396c79b18aa896
Marjan <marjan.safvati@student.uts.edu.au>
2020-08-16 21:30:20 +1000

    api-notWorking

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ window.addEventListener("load", () => {
 
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition((position) => {
-      console.log(position);
+      //console.log(position);
       long = position.coords.longitude;
       lat = position.coords.latitude;
 
       const proxy = "https://cors-anywhere.herokuapp.com/";
-      const api = `${proxy}https://api.darksky.net/forecast/fd9d9c6418c23d94745b836767721ad1/${lat}, ${long}`;
+      const api = `${proxy}api.openweathermap.org/data/2.5/weather?lat=${lat}, &long=${long}&appid={3d8581870e572c9d774887e42aaa9d37}`;
+
+      //const api = `${proxy}https://api.darksky.net/forecast/fd9d9c6418c23d94745b836767721ad1/${lat}, ${long}`;
       fetch(api)
         .then((response) => {
           return response.json();
